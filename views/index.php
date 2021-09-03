4f9a3eadc9bf00879fec4205cd8e744b841142e8
Erik Uus <erik.uus@gmail.com>
2021-09-03 14:39:29 +0300

    Add two-column layout

--- views/index.php
+++ views/index.php
@@ -1,15 +1,27 @@
 <!DOCTYPE html>
-<html>
+<html class="m-4">
 <head>
 	<title></title>
 	<link rel="stylesheet" href="../style.css">
 </head>
 <body>
-	<h1 class="mx-10 my-5">Docs</h1>
-	<?php foreach(array_diff(scandir('../docs'), array('..', '.')) as $file): ?>
-		<a target="_blank" class="block mx-10 my-1 text-blue-500" href="../docs/<?php echo $file ?>">
-			<?php echo $file ?>
-		</a>
-	<?php endforeach; ?>
+	<div class="flex space-x-4">
+		<div class="flex-1 bg-gray-100 p-4">
+			<h1>Docs</h1>
+			<?php foreach(array_diff(scandir('../docs'), array('..', '.')) as $file): ?>
+				<a target="_blank" class="block mx-10 my-1 text-blue-500" href="../docs/<?php echo $file ?>">
+					<?php echo $file ?>
+				</a>
+			<?php endforeach; ?>
+		</div>
+		<div class="flex-1 bg-gray-100 p-4">
+			<h1>Labs</h1>
+			<?php foreach(array_diff(scandir('../labs'), array('..', '.')) as $file): ?>
+				<a target="_blank" class="block mx-10 my-1 text-blue-500" href="../labs/<?php echo $file ?>">
+					<?php echo $file ?>
+				</a>
+			<?php endforeach; ?>
+		</div>
+	</div>
 </body>
 </html>
