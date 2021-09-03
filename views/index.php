0c5a896eae7cdd00b2a333165ace8a1b9842ec31
Erik Uus <erik.uus@gmail.com>
2021-09-03 13:43:39 +0300

    Rename example files

--- views/index.php
+++ views/index.php
@@ -7,7 +7,9 @@
 <body>
 	<h1 class="mx-10 my-5">Docs</h1>
 	<?php foreach(array_diff(scandir('../docs'), array('..', '.')) as $file): ?>
-	<a target="_blank" class="block mx-10 my-1 text-blue-500" href="../docs/<?php echo $file ?>"><?php echo $file ?></a>
+		<a target="_blank" class="block mx-10 my-1 text-blue-500" href="../docs/<?php echo $file ?>">
+			<?php echo $file ?>
+		</a>
 	<?php endforeach; ?>
 </body>
 </html>
