0b2483b1385ae3cd7659ee161d17bc70d5ef9b8a
Erik Uus <erik.uus@gmail.com>
2021-12-17 15:30:54 +0200

    Add new examples

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,7 +17,7 @@ module.exports = {
   plugins: [
     //require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
-    require('@tailwindcss/aspect-ratio'),
+    //require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/line-clamp')
   ],
   theme: {
