0c701b9b1c47d86aff580c7492589fab7f083fda
Erik Uus <erik.uus@gmail.com>
2025-05-21 15:31:09 +0300

    Simplify front page

--- views/index.php
+++ views/index.php
@@ -5,35 +5,21 @@
 	<link rel="stylesheet" href="../style.css">
 </head>
 <body>
-	<div class="flex space-x-4">
-		<div class="flex-1 bg-gray-100 p-4">
+	<div class="grid grid-cols-4 gap-x-4">
+		<div class="col-span-3 bg-gray-100 p-4">
 			<h1>Docs</h1>
+			<div class="columns-4 gap-x-2">
 			<?php foreach(array_diff(scandir('../docs'), array('..', '.')) as $file): ?>
 				<a target="_blank" class="block my-1 text-blue-500" href="../docs/<?php echo $file ?>">
 					<?php echo $file ?>
 				</a>
 			<?php endforeach; ?>
+			</div>
 		</div>
-		<div class="flex-1 bg-gray-100 p-4">
+		<div class="bg-gray-100 p-4">
 			<h1>Plugins</h1>
 			<?php foreach(array_diff(scandir('../plugins'), array('..', '.')) as $file): ?>
 				<a target="_blank" class="block my-1 text-blue-500" href="../plugins/<?php echo $file ?>">
-					<?php echo $file ?>
-				</a>
-			<?php endforeach; ?>
-		</div>
-		<div class="flex-1 bg-gray-100 p-4">
-			<h1>Labs</h1>
-			<?php foreach(array_diff(scandir('../labs'), array('..', '.')) as $file): ?>
-				<a target="_blank" class="block my-1 text-blue-500" href="../labs/<?php echo $file ?>">
-					<?php echo $file ?>
-				</a>
-			<?php endforeach; ?>
-		</div>
-		<div class="flex-1 bg-gray-100 p-4">
-			<h1>Private</h1>
-			<?php foreach(array_diff(scandir('../private'), array('..', '.')) as $file): ?>
-				<a target="_blank" class="block my-1 text-blue-500" href="../private/<?php echo $file ?>">
 					<?php echo $file ?>
 				</a>
 			<?php endforeach; ?>
