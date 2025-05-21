991db22211afacf9d2fc653d531a773dc8373b89
Erik Uus <erik.uus@gmail.com>
2025-05-21 15:30:45 +0300

    Upgrade to v4

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,39 +1,22 @@
-// const colors = require('tailwindcss/colors')
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  // presets: [
-  //   require('./custom.preset.js')
-  // ],
-  // corePlugins: {
-  //   preflight: false,
-  // },
-  mode: 'jit',
   content: [
-    './docs/*.html',
-    './plugins/*.html',
-    './labs/*.html',
-    './comps/*.html',
-    './private/*.html',
-    './views/*.php'
+    './docs/**/*.html',
+    './plugins/**/*.html',
+    './views/**/*.php',
+    './tailwind.css'
   ],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/line-clamp')
+    require('@tailwindcss/line-clamp'),
   ],
-  theme: {
-    extend : {
-      fontFamily: {
-        inter: 'Inter, sans-serif'
-      }
-    }
-    // colors: {
-    //   gray: colors.stone,
-    //   blue: colors.cyan,
-    //   red: colors.fuchsia,
-    //   white: colors.white
-    // }
-  },
-  variants: {},
 }
